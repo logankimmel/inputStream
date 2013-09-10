143e8643c907a1d64d27a337fd594aa67b9f49bf
MaxwellVizion <logan.kimmel@traxtech.com>
2013-09-10 11:11:03 -0700

    working file IO

--- ioFile.groovy
+++ ioFile.groovy
@@ -1,17 +1,16 @@
 File fileIn = new File("badfile.edi")
+
 FileInputStream fis = new FileInputStream(fileIn)
 BufferedInputStream bis = new BufferedInputStream(fis)
-StringBuffer strb = new StringBuffer()
+
+def bucket = []
+
 int fs = fileIn.length()
-
-for(int x = 0;x < fs;x++) { 
-	strb.append((char) bis.read())
-}
-
-String strOut = strb.toString()
-println strOut
-
 File fileOut = new File("badout.edi")
 FileOutputStream fos = new FileOutputStream(fileOut)
 
-fos.write(strOut.getBytes())
+for(int x = 0;x < fs;x++) { 
+	bucket.add((char) bis.read())
+}
+
+fos.write(bucket as byte[])
